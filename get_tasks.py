336130213cddc1c468d262f44506281b8f1f234c
alexanderys <alexander.yararsellgren@student.kyh.se>
2024-05-02 07:02:51 +0200

    cleaned up code, created class Task

--- get_tasks.py
+++ get_tasks.py
@@ -1,56 +1,23 @@
 import kanboard
-#from kanboard.task import Task
+
+# Personal API access token: "161879c114609f1674d2cf1f215dfd358501e1bff83613fe8c6fd9cb3309"
 
 # Access the Kanban board
-"""
-1)
-board = kanboard.Kanboard('http://localhost:8080/jsonrpc.php',
-                          '347a020cb5ce709441aa42b2d5652fbb8b02e477104d1d9789f7b2d40df0')
-
-2)
-kb = kanboard.Client('http://localhost/jsonrpc.php', 'admin', 'secret')
-
-3)
-kb = kanboard.Client(url='https://example.org/jsonrpc.php',
-                     username='admin',
-                     password='secret',
-                     cafile='/path/to/my/cert.pem')
-
-4) Not secure?
-kb = kanboard.Client(url='https://example.org/jsonrpc.php',
-                     username='admin',
-                     password='secret',
-                     insecure=True)
-"""
-
-# my personal API access token: "161879c114609f1674d2cf1f215dfd358501e1bff83613fe8c6fd9cb3309"
-
-# I am not getting access
-
 kb = kanboard.Client(url='http://cgi-kanban.nrm.se/jsonrpc.php',
                     username = 'alexyara',
                     password = '161879c114609f1674d2cf1f215dfd358501e1bff83613fe8c6fd9cb3309')
 
-
 """
-board = kanboard.Kanboard('http://cgi-kanban.nrm.se/jsonrpc.php', 
-                            '161879c114609f1674d2cf1f215dfd358501e1bff83613fe8c6fd9cb3309')
-
-project = board.get_project_by_name('Biodiv projekt')
-print(project)
-"""
-#print(kb)
+# Get all projects
 projects = kb.get_my_projects()
 print(projects)
-
-#print(help(kanboard))
-
-print('- - - - - - - - - - - -')
+"""
+#print('- - - - - - - - - - - -')
 """
 project = kb.get_project_by_id(project_id=1)
 print(project)
 """
-print('- - - - - - - - - - - -')
+#print('- - - - - - - - - - - -')
 
 #tasks = kb.get_all_tasks(project_id=1, status_id=0)
 #print(tasks)
@@ -60,5 +27,11 @@ print('- - - - - - - - - - - -')
 subtasks = kb.get_all_subtasks(task_id=315)
 print(subtasks)
 
-#for task in tasks:
-#    print(task.title)
+class Tasks:
+    def __init__(self, dnr, subtask_received, subtask_extracted, subtask_sent-away, subtask_analysis):
+        self.dnr = dnr
+        self.subtask_received = subtask_received
+        self.subtask_extracted = subtask_extracted
+        self.subtask_sent-away = subtask_sent-away
+        self.subtask_analysis = subtask_analysis
+
