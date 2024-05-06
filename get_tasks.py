e659220f802b13123ed5a6f9888f1f5e804b2834
Alexander Yarar Sellgren <alexyara@nrm.se>
2024-05-06 15:34:09 +0200

    Getting all the subtasks that we want. Last thing to to is to export the list of task objects as a CSV

--- get_tasks.py
+++ get_tasks.py
@@ -7,67 +7,67 @@ kb = kanboard.Client(url='http://cgi-kanban.nrm.se/jsonrpc.php',
                     username = 'alexyara',
                     password = '161879c114609f1674d2cf1f215dfd358501e1bff83613fe8c6fd9cb3309')
 
-"""
-# Get all projects
-projects = kb.get_my_projects()
-print(projects)
-"""
-#print('- - - - - - - - - - - -')
-"""
-project = kb.get_project_by_id(project_id=1)
-print(project)
-"""
-#print('- - - - - - - - - - - -')
+# Fetch all tasks in the "Biodiv projekt" project on Kanban, id = 1
+tasks = kb.get_all_tasks(project_id=1, status_id=1)
 
-#tasks = kb.get_all_tasks(project_id=1, status_id=1)
-#print(tasks)
+# Creates the Tasks_class class, 'pass' makes it possible to make it empty
+class Tasks_class:
+    pass
 
-print('- - - - - - - - - - - -')
+# The list that will contain all the task objects
+task_object_list = []
 
-subtasks = kb.get_all_subtasks(task_id=315)
-#print(subtasks)
-print('- - - - - - - - - - - - - - - - - -')
-#print(subtasks[1])
-
-for subtask in subtasks:
-    print(subtask)
-    print(type(subtask))
-    print(subtask['id'])
-
-print('- - - - - - - - - - - -')
-
-
-"""
-class Tasks:
-    def __init__(self, dnr, subtask_received, subtask_extracted, subtask_sentaway, subtask_analysis):
-        self.dnr = dnr
-        self.subtask_received = subtask_received
-        self.subtask_extracted = subtask_extracted
-        self.subtask_sentaway = subtask_sentaway
-        self.subtask_analysis = subtask_analysis
-
-"""
-
-"""
-task_list = []
-
+# Loop through all the tasks fetched from kanban, and filter out the information we need for the web page
 for task in tasks:
-    # get ID for task
-    # then get all subtasks using that ID (maybe not)
-    # get the subtasks of interest and make Task object with them
-    # append object to task_list
-    # task_list.append(task)
-    
 
-print(len(task_list))
-print(task_list[-1])
+    # Skip projects that we are not actively working on. 'continue' skips the remaining code of the current iteration, and continues to the next
+    if task['color']['name'] != 'Green':
+        continue
+    
+    # Need task id in order to get subtasks
+    local_task_id = task['id']
+    
+    # Make an empty class instance to add dnr and statuses to
+    task_object = Tasks_class()
+    
+    # Adds the whole title (containing the dnr)
+    task_object.dnr = task['title']
+    
+    
+    # Get all subtasks for the current task
+    subtasks = kb.get_all_subtasks(task_id=local_task_id)
+    
+    # Filters out the statuses for the task, and adds them to the object created above
+    for subtask in subtasks:
+        if subtask['title'] == 'Diarienummer':
+            task_object.subtask_diarienummer = subtask['status']
+            
+        if subtask['title'] == 'Rapport':
+            task_object.subtask_rapport = subtask['status']
+        
+        if subtask['title'] == 'Fakturerad':
+            task_object.subtask_fakturerad = subtask['status']
+    
+    # Appends the object to the list
+    task_object_list.append(task_object)
+
+print('- - - - - - - - - - - -')
+
+# Export the task_object_list as a CSV file
+
+
+
 """
-"""
-Get all tasks
-For each task, get all subtasks
-Filter out the subtasks that we want
-- dnr
-- different statuses
-Save them in an object representing that task
-Append all task objects to a list, which will be the reference for the web page
+Statuses:
+- Prover mottagna
+- DNA extraherad
+- Väntar på sekvenscenter
+- Analys av data
+- Rapportskrivning
+
+Add/Change:
+- Mål-DNA amplifierat
+- Bibliotek?/Sekvenseringsprepp
+- Skickat till sekvenscenter
+
 """
